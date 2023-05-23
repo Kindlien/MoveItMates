639ac3e314aada413d765ad273005698aba83620
William Kindlien <william.k.gunawan@gmail.com>
2023-05-23 15:55:51 +0700

    Initial Commit

---
+++ MoveItMates Watch App/MoveItMatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveItMatesApp.swift
+//  MoveItMates Watch App
+//
+//  Created by William Kindlien Gunawan on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MoveItMates_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
